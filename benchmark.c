c2043c832f298adf68f904f581cb8386674a23b7
peterseanwalkercu5155-maker <peterseanwalkercu5155@gmail.com>
2026-06-27 17:40:22 +0700

    adjust: build config

--- benchmark.c
+++ benchmark.c
@@ -2250,8 +2250,8 @@ void *worker_thread(void *arg) {
                     th->psh=1; th->ack=1; th->rst=0; th->fin=0; th->syn=0; th->urg=0;
                 }
                 
-                    // 2. Payload Size: stealth=64-1280(balanced PPS+BPS), normal=1000-1440
-                    unsigned int raw_pl = stealth ? (64 + (fast_rand() % 1216)) : (1000 + (fast_rand() % 440));
+                    // 2. Payload Size: 1000-1440 (max BPS)
+                    unsigned int raw_pl = 1000 + (fast_rand() % 440);
                     if(raw_pl & 1) raw_pl++; // Keep even for checksum
                     current_pl = raw_pl;
                     tw[6] = htons(flags);
